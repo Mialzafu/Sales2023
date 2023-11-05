34c1b8b7c8017c7c12072481bb44e904d4d0e13e
Mialzafu <mazf3485@gmail.com>
2023-11-05 15:23:53 -0600

    Security hardcoded on Web

---
+++ Sales2023.WEB/Auth/AuthenticationProviderTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Sales2023.WEB.Auth
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var anonimous = new ClaimsIdentity();
+            var mzamoraUser = new ClaimsIdentity(new List<Claim>
+            {
+                new Claim("FirstName", "Michael"),
+                new Claim("LastName", "Zamora"),
+                new Claim(ClaimTypes.Name, "mzamora@yopmail.com"),
+                new Claim(ClaimTypes.Role, "Admin")
+            });
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(mzamoraUser)));
+        }
+    }
+}
--- Sales2023.WEB/Program.cs
+++ Sales2023.WEB/Program.cs
@@ -1,15 +1,19 @@
 using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Sales2023.WEB;
+using Sales2023.WEB.Auth;
 using Sales2023.WEB.Repositories;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7076") });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7076/") });
 builder.Services.AddScoped<IRepository, Repository>();
 builder.Services.AddSweetAlert2();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderTest>();
 
 await builder.Build().RunAsync();
